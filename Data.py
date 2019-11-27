dbe1d02b4b3eaa315945bd722de358f435c13629
Caio Serrano(HypnoS) <hycarooss@gmail.com>
2019-11-27 20:55:34 -0300

    Delete Data.py

--- Data.py
+++
@@ -1,8 +0,0 @@
-api_key = '239cc23f637b9bd3ca82377ff1abc2bf'
-language = 'en-US'
-region = 'US'
-home = "G:\PROJECTS\GIT\TMDB_ETL_PY"
-server = 'aws-adrestia.cchy0kwa6db9.sa-east-1.rds.amazonaws.com'
-database = 'STAGE'
-username = 'Admin'
-password = 'Btr17021'
